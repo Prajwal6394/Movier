4398953527e0cb3499b85174398e46001c81ce16
prajwal6394 <prajwalpandey2009@gmail.com>
2022-05-03 04:26:04 +0530

    Basic Build

--- src/App.js
+++ src/App.js
@@ -1,11 +1,33 @@
+import { useEffect, useState } from "react";
+import Movie from "./Components/Movie";
 
-import './App.css';
-
+const Main_API =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=14f4987f91047d6556cbdf8801520ff1&page1";
+const IMG_API = "https://image.tmdb.org/t/p/w1280";
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    fetch(Main_API)
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data);
+        setMovies(data.results);
+      });
+  }, []);
   return (
-    <div className="App">
-      Hello
+    <>
+      <header>
+        <input className="search"type="search" placeholder="Search"/>
+      </header>
+      <div className="movie-container">
+      
+      {movies.map((movie) => (
+        <Movie key={movie.id} {...movie} />
+      ))}
     </div>
+    </>
+    
   );
 }
 
