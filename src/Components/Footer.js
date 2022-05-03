036606b78e9ea4d159806b31832b0b36dbccf00e
prajwal6394 <prajwalpandey2009@gmail.com>
2022-05-03 16:18:47 +0530

    footer correction

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => (
   <footer class="footer">
-    <p className="footer-p">This app display all recent top rated movies. And also allows user to search specific movie with their overviewand rating as well</p>
+    <p className="footer-p">This app displays all recent top rated movies. And also allows user to search specific movie with their overview and rating as well.</p>
     <p className="footer-q"> Made by Prajwal Pandey</p>
   </footer>
 );
